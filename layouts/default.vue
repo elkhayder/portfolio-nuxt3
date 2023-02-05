d35241da5b2bb77febec9b1fb915965becda2553
EL KHADYER Zakaria <zelkhayder@gmail.com>
2023-02-05 20:25:33 +0100

    More optimisations

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,22 @@
 <template>
    <Html :lang="head.htmlAttrs!.lang" :dir="head.htmlAttrs!.dir">
       <Head>
-         <template v-for="link in head.link" :key="link.id">
-            <Link
-               :id="link.id"
-               :rel="link.rel"
-               :href="link.href"
-               :hreflang="link.hreflang"
-            />
-         </template>
-         <template v-for="meta in head.meta" :key="meta.id">
-            <Meta
-               :id="meta.id"
-               :property="meta.property"
-               :content="meta.content"
-            />
-         </template>
+         <Link
+            v-for="link in head.link"
+            :key="link.id"
+            :id="link.id"
+            :rel="link.rel"
+            :href="link.href"
+            :hreflang="link.hreflang"
+         />
+         <Meta
+            v-for="meta in head.meta"
+            :key="meta.id"
+            :id="meta.id"
+            :property="meta.property"
+            :content="meta.content"
+         />
+         <Link rel="preload" as="image" href="/images/background-bw.jpg" />
       </Head>
       <Body>
          <Header />
