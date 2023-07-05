24ee1e14202a1e41cdbbd58281eb958d3d95807f
elkhayder <zelkhayder@gmail.com>
2023-07-05 14:16:01 +0200

    Changed age calc from round to floor

--- include/helpers.ts
+++ include/helpers.ts
@@ -1,5 +1,5 @@
 export function diffYears(dt2: Date, dt1: Date) {
    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
    diff /= 60 * 60 * 24;
-   return Math.abs(Math.round(diff / 365.25));
+   return Math.abs(Math.floor(diff / 365.25));
 }
