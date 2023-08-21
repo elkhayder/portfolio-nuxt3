8dcefb548fe3239bae8f7ef5e9f589b2dd704526
elkhayder <zelkhayder@gmail.com>
2023-08-21 06:50:49 +0200

    Added Min & Max viewport scale

--- app.vue
+++ app.vue
@@ -8,6 +8,10 @@ useHead({
 <template>
    <Head>
       <Meta
+         name="viewport"
+         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
+      />
+      <Meta
          name="description"
          content="Full-Stack Web developer & Electronics engineering student"
       />
