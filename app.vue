6dbc87244f140e782d961064544d510a56080b01
elkhayder <zelkhayder@gmail.com>
2023-12-16 20:48:54 +0100

    Updated languages

--- app.vue
+++ app.vue
@@ -27,10 +27,10 @@ useHead({
    </Head>
 
    <NuxtLayout>
+      <LoadingScreen />
+
       <NuxtPage />
    </NuxtLayout>
-
-   <LoadingScreen />
 
    <LazyClientOnly>
       <MouseFollower />
--- components/IndexPage/SpokenLanguages.vue
+++ components/IndexPage/SpokenLanguages.vue
@@ -21,11 +21,22 @@ ar:
 <template>
    <section>
       <h3 class="subtitle">{{ t("title") }}</h3>
-      <div class="grid grid-cols-3 gap-12 max-sm:gap-x-8">
+      <div
+         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-4 max-sm:gap-x-8"
+      >
          <div v-for="lang of SpokenLanguages" class="flex justify-between">
             <span class="font-medium italic">{{ lang.title }}:</span>
-            <span class="font-bold text-accent-600" title="Proficiency Level">
-               {{ lang.proficiency }}
+            <!-- <span
+               class="font-bold text-accent-600 lg:hidden"
+               title="Proficiency Level"
+            >
+               {{ lang.proficiency.short }}
+            </span> -->
+            <span
+               class="font-bold text-sm text-accent-600"
+               :title="lang.proficiency.short"
+            >
+               {{ lang.proficiency.long }}
             </span>
          </div>
       </div>
@@ -37,21 +48,33 @@ const { t } = useI18n();
 
 type SpokenLanguage = {
    title: string;
-   proficiency: "A1" | "A2" | "B1" | "B2" | "C1" | "C2";
+   proficiency: {
+      short: string;
+      long: string;
+   };
 };
 
 const SpokenLanguages: SpokenLanguage[] = [
    {
       title: t("en"),
-      proficiency: "C1",
+      proficiency: {
+         short: "C2",
+         long: "Fairly Fluent",
+      },
    },
    {
       title: t("fr"),
-      proficiency: "B2",
+      proficiency: {
+         short: "C1",
+         long: "Excellent working knowledge",
+      },
    },
    {
       title: t("ar"),
-      proficiency: "C2",
+      proficiency: {
+         short: "C2",
+         long: "Native language",
+      },
    },
 ];
 </script>
